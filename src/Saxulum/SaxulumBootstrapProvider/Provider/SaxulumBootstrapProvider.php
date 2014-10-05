a4850ec743111fdd04b337e881e15ca50d73cc55
Dominik Zogg <dominik.zogg@gmail.com>
2014-10-05 10:08:22 +0200

    a version for pimple service provider

--- src/Saxulum/SaxulumBootstrapProvider/Provider/SaxulumBootstrapProvider.php
+++ src/Saxulum/SaxulumBootstrapProvider/Provider/SaxulumBootstrapProvider.php
@@ -2,6 +2,8 @@
 
 namespace Saxulum\SaxulumBootstrapProvider\Provider;
 
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
 use Saxulum\SaxulumBootstrapProvider\Form\Extension\BootstrapExtension;
 use Saxulum\SaxulumBootstrapProvider\Form\Extension\ButtonTypeExtension;
 use Saxulum\SaxulumBootstrapProvider\Form\Extension\InputGroupButtonExtension;
@@ -12,36 +14,35 @@ use Saxulum\SaxulumBootstrapProvider\Twig\BootstrapBadgeExtension;
 use Saxulum\SaxulumBootstrapProvider\Twig\BootstrapFormExtension;
 use Saxulum\SaxulumBootstrapProvider\Twig\BootstrapIconExtension;
 use Saxulum\SaxulumBootstrapProvider\Twig\BootstrapLabelExtension;
-use Silex\Application;
 
-class SaxulumBootstrapProvider
+class SaxulumBootstrapProvider implements ServiceProviderInterface
 {
-    public function register(\Pimple $container)
+    public function register(Container $container)
     {
         $container['bootstrap.template_dir'] = __DIR__ . '/../Resources/views';
         $container['bootstrap.icon_prefix'] = 'glyphicon';
         $container['bootstrap.icon_tag'] = 'span';
 
-        $container['bootstrap.flash'] = $container->share(function () use ($container) {
+        $container['bootstrap.flash'] = function () use ($container) {
             return new FlashMessage($container['session']);
-        });
+        };
 
-        $container['form.type.extensions'] = $container->share($container->extend('form.type.extensions', function ($extensions) use ($container) {
+        $container['form.type.extensions'] = $container->extend('form.type.extensions', function ($extensions) use ($container) {
             $extensions[] = new ButtonTypeExtension();
             $extensions[] = new InputGroupButtonExtension();
             $extensions[] = new StaticControlExtension();
             $extensions[] = new TypeSetterExtension();
 
             return $extensions;
-        }));
+        });
 
-        $container['form.extensions'] = $container->share($container->extend('form.extensions', function ($extensions) use ($container) {
+        $container['form.extensions'] = $container->extend('form.extensions', function ($extensions) use ($container) {
             $extensions[] = new BootstrapExtension();
 
             return $extensions;
-        }));
+        });
 
-        $container['twig'] = $container->share($container->extend('twig', function (\Twig_Environment $twig) use ($container) {
+        $container['twig'] = $container->extend('twig', function (\Twig_Environment $twig) use ($container) {
             $twig->addExtension(new BootstrapIconExtension(
                 $container['bootstrap.icon_prefix'],
                 $container['bootstrap.icon_tag'])
@@ -51,22 +52,14 @@ class SaxulumBootstrapProvider
             $twig->addExtension(new BootstrapFormExtension());
 
             return $twig;
-        }));
+        });
 
-        $this->addTemplates($container);
-    }
-
-    /**
-     * @param Application $container
-     */
-    protected function addTemplates(Application $container)
-    {
-        $container['twig.loader.filesystem'] = $container->share($container->extend('twig.loader.filesystem',
+        $container['twig.loader.filesystem'] = $container->extend('twig.loader.filesystem',
             function (\Twig_Loader_Filesystem $twigLoaderFilesystem) use ($container) {
                 $twigLoaderFilesystem->addPath($container['bootstrap.template_dir'], 'SaxulumBootstrapProvider');
 
                 return $twigLoaderFilesystem;
             }
-        ));
+        );
     }
 }
